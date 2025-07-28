5701de2e2aec08b879006684b64908ef2db73d95
John Green <jwgreen@adobe.com>
2025-07-28 17:19:36 -0400

    test div

--- blocks/businesscard/businesscard.js
+++ blocks/businesscard/businesscard.js
@@ -1,14 +1,6 @@
 export default function decorate(block) {
-  // setup image teser
-  block.querySelector('p > a')?.classList.add('businesscard-link');
-  [...block.children].forEach((row) => {
-    const pic = row.querySelector('picture');
-    if (pic) {
-      const picWrapper = pic.parentElement.parentElement;
-      if (picWrapper && picWrapper.children.length === 1) {
-        // picture is only content in column
-        picWrapper.classList.add('businesscard-img-col');
-      }
-    }
-  });
+  block.innerHTML = '';
+  const div = document.createElement('div');
+  div.innerHTML = 'Hi From the Div';
+  block.append(div);
 }
