11a6e87290517ae70bf1d9a2653d5ce3bf5e1966
John Green <jwgreen@adobe.com>
2025-08-04 18:11:48 -0400

    updated video

--- blocks/video-ad/video-ad.js
+++ blocks/video-ad/video-ad.js
@@ -51,26 +51,21 @@ export default function decorate(block) {
 
   // Populate using the helper function
   radioButtonsContainer.append(createPersonCard(
-    'Don Stolper',
-    'https://s7d1.scene7.com/is/image/ADBDEMO/Don Stolper-1',
-    'ADBDEMO/Don Stolper-1',
+    'Susan Johnson',
+    'https://s7d1.scene7.com/is/image/ADBDEMO/agent_picture?$Responsive$',
+    'ADBDEMO/agent_picture',
   ));
   radioButtonsContainer.append(createPersonCard(
-    'Brian Stolper',
-    'https://s7d1.scene7.com/is/image/ADBDEMO/Brian%20Stolper?$Responsive$',
-    'ADBDEMO/Brian Stolper-1',
+    'Susan Johnson',
+    'https://s7d1.scene7.com/is/image/ADBDEMO/agent_picture-3?$Responsive$',
+    'ADBDEMO/agent_picture-3',
   ));
   radioButtonsContainer.append(createPersonCard(
-    'Sharon Sullivan',
-    'https://s7d1.scene7.com/is/image/ADBDEMO/Sharon Sullivan-1?$Responsive$',
-    'Sharon Sullivan-1',
+    'Susan Johnson',
+    'https://s7d1.scene7.com/is/image/ADBDEMO/agent_picture-1?$Responsive$',
+    'ADBDEMO/agent_picture-1',
   ));
-  radioButtonsContainer.append(createPersonCard(
-    'William Frank',
-    'https://s7d1.scene7.com/is/image/ADBDEMO/William Frank-1?$Responsive$',
-    'ADBDEMO/William Frank-1',
-  ));
-
+  
   form.append(radioButtonsContainer);
 
   // ---
@@ -87,19 +82,36 @@ export default function decorate(block) {
 
   // First office option (selected by default)
   const option1 = document.createElement('option');
-  option1.value = '123';
-  option1.textContent = '123 Main Street';
+  option1.value = '1';
+  option1.textContent = '2807 N Broadway, Los Angeles';
   option1.selected = true; // Set as selected by default
   select.append(option1);
 
   // Second office option
   const option2 = document.createElement('option');
-  option2.value = '424';
-  option2.textContent = '424 Hadley Drive, Suite F';
+  option2.value = '2';
+  option2.textContent = '5513 S Eastern Ave, Las Vegas';
   select.append(option2);
 
   dropdownLabel.append(select);
   formControlsDiv.append(dropdownLabel);
+
+  //  ---
+  // Add phone number
+  const phoneLabel = document.createElement('label');
+  phoneLabel.setAttribute('for', 'dynamicInput'); // Associate label with input
+  phoneLabel.textContent = 'Phone: '; // Set label text
+  const phoneInput = document.createElement('input');
+  phoneInput.type = 'text'; // Set the type to 'text'
+  phoneInput.id = 'phone'; // Optional: Set an ID
+  phoneInput.placeholder = ''; // Optional: Add a placeholder
+
+  phoneLabel.append(phoneInput);
+  formControlsDiv.append(phoneLabel);
+
+  // ---
+  // add the tag line
+  const tagLine = 'State Farm Mutual Automobile Insurance Company | State Farm Indemnity Company | State Farm Fire and Casualty Company | Bloomington, IL State Farm County Mutual Insurance Company of Texas | Richardson, TX';
 
   // ---
   // Add the 'Preview' button
@@ -128,6 +140,29 @@ export default function decorate(block) {
     previewButton.disabled = !(isImageSelected && isOfficeSelected);
   };
 
+   // Create checkbox
+  const termsLabel = document.createElement('label');
+  termsLabel.setAttribute('for', 'checkbox'); // Associate label with input
+  const approveDiv = document.createElement('div');
+  approveDiv.className = 'form-controls';
+  termsLabel.textContent = 'By clicking this checkbox, you approve the order as you have entered.   Press the Add to cart button to finish the process.';
+  const approveCheckbox = document.createElement('input');
+  approveCheckbox.type = 'checkbox';
+  approveCheckbox.id = 'acceptCheckbox';
+  
+  // Create button
+  const addButton = document.createElement('button');
+  addButton.textContent = 'Add to cart';
+  addButton.style.display = 'none'; // Initially hidden
+  addButton.className = 'preview-button';
+  approveCheckbox.addEventListener('change', () => {
+      addButton.style.display = approveCheckbox.checked ? 'block' : 'none';
+    });
+  termsLabel.appendChild(approveCheckbox);
+  approveDiv.appendChild(termsLabel);
+  approveDiv.appendChild(addButton);
+  block.append(approveDiv);
+
   // Add event listeners for form changes
   form.querySelectorAll('input[name="designOption"]').forEach((radio) => {
     radio.addEventListener('change', checkFormCompletion);
@@ -146,47 +181,27 @@ export default function decorate(block) {
     if (selectedOption) {
       pictureURL = `$picture=${selectedOption.value}&`;
     }
-
-    let agentName = '$name=';
+    const newLine = '%5Cline%20';
+    let agentName = '$name=Susan Johnson&';
     let address = '$address=';
-    let phoneNumber = '$phone=';
-    let emailAddress = '$email=';
+    let phoneNumber = phoneInput.value;
+    
 
     switch (selectedOffice.value) {
-      case '123':
-        address = '$address=123 Main Street&$city=St. Paul, MN&$zip=55112-9583&';
+      case '1':
+        address += '2807 N Broadway' + newLine + 'Los Angeles, CA 90031';
         break;
-      case '424':
-        address = '$address=424 Hadley Drive, Suite F&$city=Manhattan Beach, CA&$zip=90266-1671&';
-        break;
-      default:
-    }
-
-    switch (selectedOption?.value) {
-      case 'ADBDEMO/William Frank-1':
-        agentName += 'William Frank&';
-        phoneNumber += '727.789.1234&';
-        emailAddress += 'wfrank@statefarm.com';
-        break;
-      case 'Sharon Sullivan-1':
-        agentName += 'Sharon Sullivan&';
-        phoneNumber += '813.235.8521&';
-        emailAddress += 'ssullivan@statefarm.com';
-        break;
-      case 'ADBDEMO/Brian Stolper-1':
-        agentName += 'Brian Stolper&';
-        phoneNumber += '404.687.5468&';
-        emailAddress += 'bstolper@statefarm.com';
-        break;
-      case 'ADBDEMO/Don Stolper-1':
-        agentName += 'Don Stolper&';
-        phoneNumber += '305.348.9876&';
-        emailAddress += 'dstolper@statefarm.com';
+      case '2':
+        address += '5513 S Eastern Ave' + newLine + 'Las Vegas, NV 89119';
         break;
       default:
+        adress = "default";
     }
 
-    const finalURL = `${imageURL}${pictureURL}${agentName}${address}${phoneNumber}${emailAddress}`;
+ if (phoneNumber !== ''){
+        address += newLine + phoneNumber;
+    }
+    const finalURL = `${imageURL}${pictureURL}${agentName}${address}`;
     previewContainer.innerHTML = `<img src="${finalURL}">`;
     previewContainer.style.display = 'block';
   });
